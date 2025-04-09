f8ae4f7ad87e1f09282ab965d37e2989b7f946ba
QuickLR1 <bekaisain68@gmail.com>
2025-04-09 15:38:15 +0500

    Законченный интерфейс.Ровно то что требуется по заданию.

--- src/list/MyList.java
+++ src/list/MyList.java
@@ -1,4 +1,35 @@
 package list;
 
-public interface MyList {
+import java.util.Iterator;
+
+public interface MyList<T> extends Iterable<T> { // T это generic( типо универсальный тип данных,когда буду использовать буду обьявлять.), interface это что-то по типо шаблона.То есть те функции которые здесь,должны быть реализованы в любом классе.
+    void add(T item);
+    void add(int index, T item);
+    void addFirst(T item);
+    void addLast(T item);
+
+    void set(int index, T item);
+
+    T get(int index);
+    T getFirst();
+    T getLast();
+
+    T remove(int index);
+    T removeFirst();
+    T removeLast();
+
+    void sort();
+
+    int indexOf(Object object);
+    int lastIndexOf(Object object);
+
+    boolean contains(Object object);
+
+    Object[] toArray();
+    void clear();
+    int size();
+    boolean isEmpty();
+
+    @Override
+    Iterator<T> iterator();
 }

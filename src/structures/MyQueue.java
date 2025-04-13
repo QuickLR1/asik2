fbe27819685df730c86b892d6ca125f97ea74461
QuickLR1 <bekaisain68@gmail.com>
2025-04-13 10:45:46 +0500

    Осталось доделать  MyMinHeap,с ним чуть посложнее

--- src/structures/MyQueue.java
+++ src/structures/MyQueue.java
@@ -1,4 +1,29 @@
 package structures;
+import list.MyLinkedList;
+public class MyQueue<T extends Comparable<T>> {
+    private MyLinkedList<T> list = new MyLinkedList<>();
 
-public class MyQueue {
+    public void enqueue(T item) {
+        list.addLast(item);
+    }
+
+    public T dequeue() {
+        if (isEmpty()) throw new IllegalStateException("Queue is empty");
+        T item = list.getFirst();
+        list.removeFirst();
+        return item;
+    }
+
+    public T peek() {
+        if (isEmpty()) throw new IllegalStateException("Queue is empty");
+        return list.getFirst();
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public int size() {
+        return list.size();
+    }
 }
--- src/structures/MyStack.java
+++ src/structures/MyStack.java
@@ -1,6 +1,30 @@
 package structures;
+import list.MyArrayList;
 
-public class MyStack {
+public class MyStack<T extends Comparable<T>> {
+    private MyArrayList<T> list = new MyArrayList<>();
 
+    public void push(T item) {
+        list.addLast(item);
+    }
 
+    public T pop() {
+        if (isEmpty()) throw new IllegalStateException("Stack is empty");
+        T item = list.getLast();
+        list.removeLast();
+        return item;
+    }
+
+    public T peek() {
+        if (isEmpty()) throw new IllegalStateException("Stack is empty");
+        return list.getLast();
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public int size() {
+        return list.size();
+    }
 }
